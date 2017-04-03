a9afee6b755e78faf5377f9feed35cf5ba58cbc9
yuri.okulovsky <yuri.okulovsky@gmail.com>
2017-04-03 17:57:14 +0500

    Новый принтинг

---
+++ QuickAnalysis/Printing1.cs
@@ -0,0 +1,114 @@
+﻿using DataScience.Tables;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataScience.QuickAnalysis
+{
+    public static class Printing1
+    {
+        static Dictionary<Type, List<IAccessor>> accessors = new Dictionary<Type, List<IAccessor>>();
+
+
+        static Dictionary<string,object> FlattenObject(object e)
+        {
+            var type = e.GetType();
+            if (!accessors.ContainsKey(type))
+                accessors[type] = type.GetHierarchicalAccessors().ToList();
+            return accessors[type].ToDictionary(z => z.Name, z => z.GetValue(e));
+        }
+
+        static IEnumerable<DictionaryEntry> GetEntries(this  IDictionary d)
+        {
+            foreach (var e in d) yield return (DictionaryEntry)e;
+        }
+
+        public static IEnumerable<Dictionary<string, object>> Flatten(object data)
+        {
+            if (data is IEnumerable)
+            {
+                foreach (var e in ((IEnumerable)data))
+                {
+                    if (e is IDictionary)
+                    {
+                       
+                        yield return ((IDictionary)e)
+                            .GetEntries()
+                            .ToDictionary(z => z.Key.ToString(), z => z.Value);
+                        continue;
+                    }
+                    if (e is IEnumerable)
+                    {
+                        yield return ((IEnumerable)e)
+                            .Cast<object>()
+                            .WithIndices()
+                            .ToDictionary(z => z.Index.ToString(), z => z.Item);
+                        continue;
+                    }
+
+                    yield return FlattenObject(e);
+                }
+            }
+            else if (data.GetType().IsPrintable())
+            {
+                yield return new Dictionary<string, object> { ["Value"] = data };
+            }
+            else
+            {
+                yield return FlattenObject(data);
+            }
+        }
+
+        public static IEnumerable<string> ToCsvStrings<T>(this IEnumerable<T> data)
+        {
+            bool firstTime = true;
+            foreach(var e in Flatten(data))
+            {
+                if (firstTime) yield return string.Join(",", e.Keys);
+                firstTime = true;
+                yield return string.Join(",", e.Values.Select(z =>
+                {
+                    if (z == null) return "NA";
+                    else if (Types.NullableTypes.Contains(z.GetType()))
+                        return z.ToString();
+                    else return $"\"{z.ToString()}\"";
+                }));
+            }
+        }
+
+        public static string Print(object data)
+        {
+            var table = new Table<int, string, string>();
+            var indexer = table.Indexer(z => z.AutoCreate(), z => z.AutoCreate());
+            foreach (var row in Flatten(data).WithIndices())
+                foreach (var column in row.Item)
+                    indexer[row.Index, column.Key] = column.Value?.ToString()??"NULL";
+
+            var lengths = table
+                .Columns
+                .WithIndices()
+                .Select(z => 
+                 new
+                 {
+                     columnIndex=z.Index,
+                     columnName=z.Item,
+                     max=table.GetColumnData(z.Item).Max(x => x.Value.Length)
+                 })
+                .ToList();
+
+            var formats = lengths
+                .Select(coldata => "{" + coldata.columnIndex + ",-" + (coldata.max + 2) + "}")
+                .Aggregate((a, b) => a + b);
+
+            var builder = new StringBuilder();
+            builder.AppendLine(string.Format(formats, lengths.Select(z=>z.columnName).ToList()));
+            builder.AppendLine(string.Format(formats, lengths.Select(z => new string('-', z.max)).ToArray()));
+            foreach (var e in table.Rows)
+                builder.AppendLine(string.Format(formats, lengths.Select(z=>table.GetValue(e,z.columnName)).ToArray()));
+            return builder.ToString();
+        }
+    }
+}
---
+++ QuickAnalysis/Printing1_should.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataScience.QuickAnalysis
+{
+    [TestFixture]
+    class Printing1_should
+    {
+        void Check(object obj, params object[] expected)
+        {
+            var data = Printing1.Flatten(obj);
+            var counter = 0;
+            foreach(var e in data.SelectMany(z=>z))
+            {
+                Assert.AreEqual(e.Key, expected[counter]);
+                Assert.AreEqual(e.Value, expected[counter + 1]);
+                counter += 2;
+            }
+            Assert.AreEqual(expected.Length, counter);
+        }
+        [Test]
+        public void FlattenObject()
+        {
+            Check(new { a = 1, b = 1.2 }, "a", 1, "b", 1.2);
+        }
+
+        [Test]
+        public void FlattenIen()
+        {
+            Check(
+                Enumerable.Range(1, 2).Select(z => new { a = z, b = 2 * z }),
+                "a", 1, "b", 2, "a", 2, "b", 4);
+        }
+
+        [Test]
+        public void FlattenIenIen()
+        {
+            Check(
+                Enumerable.Range(1, 2)
+                .Select(z => Enumerable.Range(1, z)),
+                "0", 1, "0", 1, "1", 2);
+        }
+
+        [Test]
+        public void FlattenDic()
+        {
+            Check(
+                Enumerable
+                    .Range(2, 2)
+                    .Select(z => Enumerable.Range(z, 2).ToDictionary(x => "i" + x, x => x)),
+                "i2", 2, "i3", 3, "i3", 3, "i4", 4);
+
+        }
+    }
+}
